2dd310b3e83e6e93623ba130fa892d46bd76a1c6
loukayl <79642918+loukayl@users.noreply.github.com>
2021-02-26 14:02:49 +0100

    Add files via upload

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Can be multiple prefixes, like this: ("!", "?")
 BOT_PREFIX = ("v")
-TOKEN = "NzgyNDE5MTI4NTk3NjEwNTE3.X8L6mA.sozaCSJx2K9Zba45PIjFvEFDiGw"
-APPLICATION_ID = "782419128597610517"
+TOKEN = ""
+APPLICATION_ID = ""
 OWNERS = [674700670929993769, 460129131783389184]
 BLACKLIST = []
  # Default cogs that I have created for the template
